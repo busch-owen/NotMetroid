c6ef2697b4704472b027951f4e445843a37d8cb2
busch-owen <buschowen@gmail.com>
2024-07-18 12:13:27 -0600

    Added instructions on the MinimapMover on how to use

--- NOTMetroid/Assets/Scripts/UI/MinimapMover.cs
+++ NOTMetroid/Assets/Scripts/UI/MinimapMover.cs
@@ -6,6 +6,9 @@ public class MinimapMover : MonoBehaviour
 {
     [SerializeField] private int shiftAmount;
     
+    //To use this script you must call this shift map function, which at the moment is only getting called from the SwitchCamera class
+    //Input a direction and the map will shift in that direction when you move between rooms so long as there is a trigger between them
+    
     public void ShiftMap(Vector2 direction)
     {
         transform.position = new Vector2(transform.position.x + shiftAmount * direction.x, transform.position.y + shiftAmount * direction.y);
